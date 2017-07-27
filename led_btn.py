61e757cd690b02ba279af1f12aaa9fc0b13043ef
ankitsejwal <ankitsejwal@gmail.com>
2017-07-28 00:26:27 +1000

    Led and btn basic programs

---
+++ led_btn.py
@@ -0,0 +1,25 @@
+import RPi.GPIO as GPIO
+import time
+
+# Pin definition
+
+ledPin = 18
+btnPin = 16
+
+# Setup GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(ledPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+GPIO.output(ledPin, GPIO.HIGH)
+
+# Infinite loop
+try:
+    while 1:
+        if GPIO.input(btnPin):  #not pressed
+            GPIO.output(ledPin, GPIO.LOW)
+        else:
+            GPIO.output(ledPin, GPIO.HIGH)
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    
---
+++ led_glow.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+
+# Pin definition
+
+ledPin = 18
+pwmLed = 16
+
+duty = 70
+
+# Setup GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(ledPin, GPIO.OUT)
+GPIO.setup(pwmLed, GPIO.OUT)
+
+pwm = GPIO.PWM(pwmLed, 200) # set pwm freq = 200Hz
+GPIO.output(ledPin, GPIO.HIGH)
+pwm.start(duty)
+
+# Infinite loop
+try:
+    while 1:
+        time.sleep(0.5)
+        pwm.ChangeDutyCycle(duty)
+        GPIO.output(ledPin, GPIO.LOW)
+        time.sleep(0.5)
+        pwm.ChangeDutyCycle(100 - duty)
+        GPIO.output(ledPin, GPIO.HIGH)
+except KeyboardInterrupt:
+    pwm.stop()
+    GPIO.cleanup()
