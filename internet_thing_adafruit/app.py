63b7d4d7fa20923aa9eeef6ed00e5cf6eb4a3408
ankitsejwal <ankit@sejw.al>
2018-06-21 03:11:20 +1000

    setup internet things project

---
+++ internet_thing_adafruit/app.py
@@ -0,0 +1,11 @@
+#!bin/python3
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+app.run(port=5000, debug=True)
