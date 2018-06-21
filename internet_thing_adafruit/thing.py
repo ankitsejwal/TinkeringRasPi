e049c2cc2d5d543b46b8af3e9c519a9585cd2470
ankitsejwal <ankit@sejw.al>
2018-06-22 01:44:25 +1000

    pithing class

--- internet_thing_adafruit/thing.py
+++ internet_thing_adafruit/thing.py
@@ -1,12 +1,19 @@
 import RPi.GPIO as GPIO 
 
+LED_PIN     = 23
+SWITCH_PIN  = 24
+
 class PiThing(object):
 
     def __init__(self):
-        pass
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(LED_PIN, GPIO.OUT) # led as an output
+        GPIO.setup(SWITCH_PIN, GPIO.IN)  # switch as an input
 
     def read_switch(self):
-        pass
+        ''' read the state of the switch '''
+        return GPIO.input(SWITCH_PIN)
 
-    def set_led(self):
-        pass
+    def set_led(self, value):
+        ''' turn led on or off by setting a value '''
+        GPIO.output(LED_PIN, value)
